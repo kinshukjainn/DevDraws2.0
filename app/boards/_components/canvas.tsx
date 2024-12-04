b39140118642024ca87ef7c733d40b383b5988a1
Kumar Arnim <kumararnim1@vivaldi.net>
2024-12-05 00:16:14 +0530

    Added Toolbar Components and states to manage Canvas

--- app/boards/_components/canvas.tsx
+++ app/boards/_components/canvas.tsx
@@ -1,10 +1,15 @@
 "use client"
 
+import { useState } from "react"
+
+import { CanvasState, CanvasMode } from "@/types/canvas"
+
 import { Info } from "./info"
 import { Participants } from "./participants"
 import { Toolbar } from "./toolbar"
 
 // import { useSelf } from "@liveblocks/react/suspense";
+import { useHistory, useCanUndo, useCanRedo } from "@liveblocks/react/suspense";
 
 interface CanvasProps{
     boardId: string;
@@ -14,17 +19,30 @@ export const Canvas = ({
     boardId
 }: CanvasProps) => {
 
-    // boardId coming from [boardId] page.tsx, search querying
-
     // const info = useSelf((me) => me.info)
     // console.log(info)
+
+    const [canvasState, setCanvasState] = useState<CanvasState>({
+        mode: CanvasMode.None
+    })
+
+    const history = useHistory()
+    const canUndo = useCanUndo()
+    const canRedo = useCanRedo()
 
     return (
         // TODO: Use Tldr {https://tldraw.dev} to enhance canvas and more features later
         <main className="h-full w-full relative bg-neutral-100 touch-none">
             <Info boardId={boardId} />
             <Participants />
-            <Toolbar />
+            <Toolbar
+                canvasState={canvasState}
+                setCanvasState={setCanvasState}
+                canRedo={canRedo}
+                canUndo={canUndo}
+                undo={history.undo}
+                redo={history.redo}
+            />
         </main>
     )
 }
